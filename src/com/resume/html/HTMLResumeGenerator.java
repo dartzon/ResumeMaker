0ff9ff40fdc401b459b8f14132d6ad7e66536fa7
Othmane AIT EL CADI <dartzon@gmail.com>
2015-12-13 00:36:49 +0000

    Education.html

--- src/com/resume/html/HTMLResumeGenerator.java
+++ src/com/resume/html/HTMLResumeGenerator.java
@@ -5,10 +5,7 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.CharBuffer;
-
 import com.resume.data.Person;
 
 public class HTMLResumeGenerator
