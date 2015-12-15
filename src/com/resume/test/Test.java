fe8f1a082f07cd22aca44916ca154734748022bc
Othmane AIT EL CADI <dartzon@gmail.com>
2015-12-15 00:48:54 +0000

    Update to main function

--- src/com/resume/test/Test.java
+++ src/com/resume/test/Test.java
@@ -1,22 +1,30 @@
 package com.resume.test;
 
+import java.util.Vector;
+
+import com.resume.data.Education;
+import com.resume.data.Experience;
 import com.resume.data.Person;
 import com.resume.html.HTMLResumeGenerator;
+import com.resume.persistance.EducationPersistance;
+import com.resume.persistance.ExperiencePersistance;
 import com.resume.persistance.PersonPersistance;
 import com.resume.persistance.SQLiteManager;
 
 public class Test
 {
     public static void main(String[] args)
-    {	
-	Person prs= new Person();	
-	
+    {
 	PersonPersistance prsper= new PersonPersistance();
+	Person prs= (Person) prsper.load(1);
 	
-	//prsper.save(prs);
-	prs= (Person) prsper.load(1);
+	EducationPersistance eduper= new EducationPersistance();
+	Vector<Education> educations= (Vector<Education>) eduper.load( prs.getID() );
 	
-	HTMLResumeGenerator gen= new HTMLResumeGenerator(prs);
+	ExperiencePersistance expper= new ExperiencePersistance();
+	Vector<Experience> experiences= (Vector<Experience>) expper.load( prs.getID() );
+	
+	HTMLResumeGenerator gen= new HTMLResumeGenerator(prs, educations, experiences);
 	gen.createIndexHTML();
 	
 	SQLiteManager.release();
